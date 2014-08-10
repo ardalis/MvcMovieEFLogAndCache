2e57ac9810ca9ef6114e10efc610d4512b3b7c2c
Steve Smith <steve@kentsmiths.com>
2014-08-09 23:13:46 -0400

    Fixed logging
    Now it works on every query, not just onmodelcreating...

--- src/MvcMovie/Models/MovieDbContext.cs
+++ src/MvcMovie/Models/MovieDbContext.cs
@@ -5,12 +5,11 @@ namespace MvcMovie.Models
 {
     public class MovieDbContext : DbContext
     {
-        public DbSet<Movie> Movies { get; set; }
+      public DbSet<Movie> Movies { get; set; }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        public MovieDbContext()
         {
             Database.Log = s => Debug.Print(s);
-            base.OnModelCreating(modelBuilder);
         }
     }
 }
