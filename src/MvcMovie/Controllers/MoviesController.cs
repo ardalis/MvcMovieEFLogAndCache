5c0ccb5b924683af9d8c5f94619156e58b567f3a
Steve Smith <steve@kentsmiths.com>
2014-08-18 23:32:51 -0400

    Reformatted

--- src/MvcMovie/Controllers/MoviesController.cs
+++ src/MvcMovie/Controllers/MoviesController.cs
@@ -19,33 +19,33 @@ namespace MvcMovie.Controllers
             _movieRepository = movieRepository;
         }
 
-        public MoviesController() : this(new EfMovieRepository())
-        {}
+        public MoviesController()
+            : this(new EfMovieRepository())
+        { }
 
         // GET: /Movies/
         public ActionResult Index(string movieGenre, string searchString)
         {
-            ViewBag.movieGenre = 
+            ViewBag.movieGenre =
                 new SelectList(_movieRepository.ListGenres());
 
-
-            return View(_movieRepository.ListMovies(movieGenre,searchString));
+            return View(_movieRepository.ListMovies(movieGenre, searchString));
         }
 
         // GET: /Movies/Details/5
-public ActionResult Details(int? id)
-{
-    if (id == null)
-    {
-        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-    }
-    Movie movie = db.Movies.Find(id);
-    if (movie == null)
-    {
-        return HttpNotFound();
-    }
-    return View(movie);
-}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
 
         // GET: /Movies/Create
         public ActionResult Create()
@@ -66,79 +66,79 @@ public ActionResult Create()
 }
 
  */
-// POST: /Movies/Create
-// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-[HttpPost]
-[ValidateAntiForgeryToken]
-public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
-{
-    if (ModelState.IsValid)
-    {
-        db.Movies.Add(movie);
-        db.SaveChanges();
-        return RedirectToAction("Index");
-    }
+        // POST: /Movies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movie);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-    return View(movie);
-}
+            return View(movie);
+        }
 
-// GET: /Movies/Edit/5
-public ActionResult Edit(int? id)
-{
-    if (id == null)
-    {
-        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-    }
-    Movie movie = db.Movies.Find(id);
-    if (movie == null)
-    {
-        return HttpNotFound();
-    }
-    return View(movie);
-}
+        // GET: /Movies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
 
-// POST: /Movies/Edit/5
-// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-[HttpPost]
-[ValidateAntiForgeryToken]
-public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
-{
-    if (ModelState.IsValid)
-    {
-        db.Entry(movie).State = EntityState.Modified;
-        db.SaveChanges();
-        return RedirectToAction("Index");
-    }
-    return View(movie);
-}
+        // POST: /Movies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
 
-// GET: /Movies/Delete/5
-public ActionResult Delete(int? id)
-{
-    if (id == null)
-    {
-        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-    }
-    Movie movie = db.Movies.Find(id);
-    if (movie == null)
-    {
-        return HttpNotFound();
-    }
-    return View(movie);
-}
+        // GET: /Movies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
 
-// POST: /Movies/Delete/5
-[HttpPost, ActionName("Delete")]
-[ValidateAntiForgeryToken]
-public ActionResult DeleteConfirmed(int id)
-{
-    Movie movie = db.Movies.Find(id);
-    db.Movies.Remove(movie);
-    db.SaveChanges();
-    return RedirectToAction("Index");
-}
+        // POST: /Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Movie movie = db.Movies.Find(id);
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
         protected override void Dispose(bool disposing)
         {
